6fa6081ccf60b8c9b7f91d979ebc0546721de952
Parn23 <77811207+Parn23@users.noreply.github.com>
2021-02-21 12:49:59 -0800

    Added Python base code

---
+++ Python/main.py
@@ -0,0 +1,11 @@
+operation = None
+input1 = None
+input2 = None
+print("             Calculator\n"
+      "--------------------------------------\n")
+print("   1) Add\n"
+      "   2) Subtract\n"
+      "   3) Multiply\n"
+      "   4) Divide\n"
+      "   5) Exponential\n"
+      "   6) Square root\n")
