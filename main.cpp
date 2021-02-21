3455011fb778e794d3226a1eb78fbe8da250e65a
Parn23 <77811207+Parn23@users.noreply.github.com>
2021-02-20 21:49:11 -0800

    Added some of the main code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    // Variable definitions
+    extern string operation;
+    // Main code
+    cout << "             Calculator\n"
+            "--------------------------------------" << endl;
+    cout << "   1) Add\n"
+            "   2) Subtract\n"
+            "   3) Multiple\n"
+            "   4) Divide" << endl;
+    cin >> operation;
 
 }
