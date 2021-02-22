088c79771d960d3acc406e14d9a60ea0857f9468
Parn23 <77811207+Parn23@users.noreply.github.com>
2021-02-21 17:07:50 -0800

    Made interface a little more user-friendly

--- Python/advanced.py
+++ Python/advanced.py
@@ -3,9 +3,9 @@ import math
 
 def expo(num1, num2):
     num3 = int(num1) ** int(num2)
-    print(num3)
+    print("Answer: " + num3)
 
 
 def sqrt(num1):
     num3 = math.sqrt(num1)
-    print(num3)
+    print("Answer: " + num3)
