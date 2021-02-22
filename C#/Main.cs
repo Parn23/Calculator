42b86c28ac623af9332698780f742ae36e4477fc
Parn23 <77811207+Parn23@users.noreply.github.com>
2021-02-21 16:24:19 -0800

    Added base code

---
+++ C#/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace CalculatorCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable definitions
+            ConsoleKeyInfo operation;
+            ConsoleKeyInfo input1;
+            ConsoleKeyInfo input2;
+            // Main code
+            WriteLine("             Calculator\n" +
+                      "--------------------------------------");
+            WriteLine(
+                      "   1) Add\n" +
+                      "   2) Subtract\n" +
+                      "   3) Multiply\n" +
+                      "   4) Divide\n" +
+                      "   5) Exponential\n" +
+                      "   6) Square root");
+            operation = ReadKey();
+        }
+    }
+}
