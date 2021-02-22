0ca27e87560c767756698304e158c64d817a1d84
Parn23 <77811207+Parn23@users.noreply.github.com>
2021-02-21 17:14:17 -0800

    C#: Added basic class for normal operations

---
+++ C#/Basic.cs
@@ -0,0 +1,32 @@
+﻿using static System.Convert;
+using static System.Console;
+
+namespace CalculatorCS
+{
+    public class Basic
+    {
+        public static void Add(int num1, int num2)
+        {
+            int num3 = num1 + num2;
+            WriteLine("Answer: " + num3);
+        }
+
+        public static void Subtract(int num1, int num2)
+        {
+            int num3 = num1 - num2;
+            WriteLine("Answer: " + num3);  
+        }
+
+        public static void Multiply(int num1, int num2)
+        {
+            int num3 = num1 * num2;
+            WriteLine("Answer: " + num3);
+        }
+
+        public static void Divide(int num1, int num2)
+        {
+            int num3 = num1 / num2;
+            WriteLine("Answer: " + num3);
+        }
+    }
+}
--- C#/Main.cs
+++ C#/Main.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using static System.Console;
+﻿using static System.Console;
+using static System.Convert;
 
 namespace CalculatorCS
 {
@@ -8,20 +8,47 @@ namespace CalculatorCS
         static void Main(string[] args)
         {
             // Variable definitions
-            ConsoleKeyInfo operation;
-            ConsoleKeyInfo input1;
-            ConsoleKeyInfo input2;
+            int operation;
+            int input1;
+            int input2;
             // Main code
             WriteLine("             Calculator\n" +
                       "--------------------------------------");
             WriteLine(
-                      "   1) Add\n" +
-                      "   2) Subtract\n" +
-                      "   3) Multiply\n" +
-                      "   4) Divide\n" +
-                      "   5) Exponential\n" +
-                      "   6) Square root");
-            operation = ReadKey();
+                "   1) Add\n" +
+                "   2) Subtract\n" +
+                "   3) Multiply\n" +
+                "   4) Divide\n" +
+                "   5) Exponential\n" +
+                "   6) Square root");
+            operation = ToInt32(ReadLine());
+            switch (operation)
+            {
+                case 1:
+                    input1 = ToInt32(ReadLine());
+                    input2 = ToInt32(ReadLine());
+                    Basic.Add(input1, input2);
+                    break;
+                case 2:
+                    input1 = ToInt32(ReadLine());
+                    input2 = ToInt32(ReadLine());
+                    Basic.Subtract(input1, input2);
+                    break;
+                case 3:
+                    input1 = ToInt32(ReadLine());
+                    input2 = ToInt32(ReadLine());
+                    Basic.Multiply(input1, input2);
+                    break;
+                case 4:
+                    input1 = ToInt32(ReadLine());
+                    input2 = ToInt32(ReadLine());
+                    Basic.Divide(input1, input2);
+                    break;
+                default:
+                    WriteLine("Invalid value. Please rerun the program and try again.");
+                    break;
+            }
+
         }
     }
 }
